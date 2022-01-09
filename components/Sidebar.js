494385d7734e10dabdcff7f8c13f6b7ff672e2f4
Shivam Shah <shiva.sean27@gmail.com>
2022-01-10 01:28:42 +0530

    commit after adding songs list

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -37,14 +37,6 @@ function Sidebar() {
 		lg:max-w-[15rem] hidden md:inline-flex'
 		>
 			<div className='space-y-4'>
-				<button
-					className='flex items-center space-x-2 hover:text-white'
-					onClick={() => signOut()}
-				>
-					<HomeIcon className='h-5 w-5' />
-					<p>Logout</p>
-				</button>
-
 				<button className='flex items-center space-x-2 hover:text-white'>
 					<HomeIcon className='h-5 w-5' />
 					<p>Home</p>
--- components/Songs.js
+++ components/Songs.js
@@ -1,7 +1,14 @@
+import { useRecoilValue } from 'recoil';
+import { playlistState } from '../atoms/playlistAtom';
+import Song from './Song';
+
 function Songs() {
+	const playlist = useRecoilValue(playlistState);
 	return (
-		<div>
-			<div>Songs</div>
+		<div className='text-white px-6 flex flex-col space-y-1 '>
+			{playlist?.tracks.items.map((track, i) => (
+				<Song track={track} order={i} />
+			))}
 		</div>
 	);
 }
