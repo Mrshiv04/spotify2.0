86b8ad7481316edb6d7aaa4d319560e099456142
Shivam Shah <shiva.sean27@gmail.com>
2022-01-19 16:25:59 +0530

    commit after adding album art, and left section of player

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
 import Center from '../components/Center';
 import { getSession } from 'next-auth/react';
+import Player from '../components/Player';
 
 export default function Home() {
 	return (
@@ -15,7 +16,9 @@ export default function Home() {
 				<Sidebar className='' />
 				<Center />
 			</main>
-			<div>{/* player */}</div>
+			<div className='sticky bottom-0'>
+				<Player />
+			</div>
 		</div>
 	);
 }
